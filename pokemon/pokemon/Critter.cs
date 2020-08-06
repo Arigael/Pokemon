b887b2d07c7b7eee79a8d9b03fc2989f2a09cad0
Arigael <toropelaez@hotmail.com>
2020-08-06 17:25:52 -0500

    third commit
    Skill Constructor & Critter Constructor

--- pokemon/pokemon/Critter.cs
+++ pokemon/pokemon/Critter.cs
@@ -6,13 +6,36 @@ namespace pokemon
 {
     class Critter
     {
-        private string name;
-        private float baseAttack;
-        private float baseDefense;
-        private float basespeed;
-        private float hp;
-        private enum affinity{};
-        private List<> moveSet;
+        private string Name;
+        private float BaseAttack;
+        private float BaseDefense;
+        private float BaseSpeed;
+        private float HP;
+        private List<> MoveSet;
         
+
+        public enum Affinity 
+        {
+            Fire, 
+            Wind,
+            Earth,
+            Dark,
+            Water,
+            Light
+        }
+        
+        private Affinity affinityType;
+
+        public Critter (string name, float baseAttack, float baseDefense, float baseSpeed, float hp, List<> moveSet,  Affinity affinity)
+        {
+            affinityType = affinity;
+            Name = name;
+            BaseAttack = baseAttack;
+            BaseDefense = baseDefense;
+            BaseSpeed = baseSpeed;
+            HP = hp;
+            MoveSet = moveSet;
+
+        }
     }
 }
--- pokemon/pokemon/Skill.cs
+++ pokemon/pokemon/Skill.cs
@@ -6,9 +6,36 @@ namespace pokemon
 {
     class Skill
     {
-        private string name;
-        private enum affinity{};
-        private enum type{};
+        private string Name;
         private float Power;
+
+        private enum Type
+        {
+            AttackSkill,
+            SupportSkill
+        }
+
+        private Type skillType;
+        
+
+        public enum Affinity 
+        {
+            Fire, 
+            Wind,
+            Earth,
+            Dark,
+            Water,
+            Light
+        }
+        
+        private Affinity affinityType;
+
+        public Skill (string name, float power, Type type, Affinity affinity)
+        {
+            Name = name;
+            Power = power;
+            skillType = Type;
+            affinityType = affinity;
+        }
     }
 }
