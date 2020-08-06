1013036ea1e168beb06d682912c9418918516c31
Arigael <toropelaez@hotmail.com>
2020-08-06 16:28:25 -0500

    First commit

---
+++ pokemon/pokemon/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pokemon
+{
+    class Player
+    {
+        private Queue<Critter> team;
+        private string playerName;
+        
+        public string PlayerName   // property
+        {
+            get { return playerName; }   // get method
+            set { playerName = value; }  // set method
+        }
+    }
+}
---
+++ pokemon/pokemon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
